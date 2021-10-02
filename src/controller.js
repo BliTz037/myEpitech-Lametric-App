50644cbb5f601d000ded99ab25c25c54a8f2ea7e
Tom Rives <tom.rives@epitech.eu>
2021-10-02 23:26:17 +0200

    [ADD] get params

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,25 @@
 const config = require('../config.json');
 
+/*
+params
+&cookie=abcdefg1234567
+&show_data=credits,netsoul,gpa,agenda
+*/
 exports.getData = (req, res) => {
-    
+    const params = req.query;
+    const dataFormat = {
+        "frames": [
+            {
+                "text": "Epitech",
+                "icon": 46869
+            }
+        ]
+    };
+    const showData = req.query.show_data.split(",")
+    console.log(params);
+    console.log(showData);
+    for (const data of showData) {
+        
+    }
+    return res.status(200).json(dataFormat)
 }
